5ee3055530e3a9dbb2ea0fab380fd8aed42da8be
Mohammed Salama <mhs170@scarletmail.rutgers.edu>
2023-10-26 15:01:52 -0400

    tabs and part of open/close

--- src/main/java/com/example/cs213projectthree/HelloApplication.java
+++ src/main/java/com/example/cs213projectthree/HelloApplication.java
@@ -13,8 +13,8 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader =
                 new FXMLLoader(HelloApplication.class.getResource("hello" +
                         "-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        stage.setTitle("Project 3 - Transaction Manager");
         stage.setScene(scene);
         stage.show();
     }
