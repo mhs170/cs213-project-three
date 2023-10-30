623ed1b584e19dd22a35a7063a01342f9fec0402
Dakshal P <dakshal29@gmail.com>
2023-10-30 10:27:02 -0400

    implement open methods

--- src/main/java/banking/TransactionManagerController.java
+++ src/main/java/banking/TransactionManagerController.java
@@ -171,15 +171,44 @@ public class TransactionManagerController {
             String accountType = accountTypePicker.getValue();
             String campus = campusPicker.getValue();
             boolean isLoyal = loyaltyCheckbox.isSelected();
-            Double amount = Double.parseDouble(amountField.getText());
+            String amountStr = amountField.getText();
+
+            //check if names are valid (not empty)
+            if(firstName.isEmpty()) {
+                print(output, "Please enter a first name");
+                return;
+            }
+            if(lastName.isEmpty()) {
+                print(output, "Please enter a last name");
+                return;
+            }
 
             //create date and check if valid
+            if(dobString.isEmpty()) {
+                print(output, "Please enter a date of birth.");
+                return;
+            }
             Date dob = createDate(dobString);
             if(!dateIsValid(dob, dobString)) return;
 
-            //run corresponding function
+            //parse amount (throws exception)
+            if(amountStr.isEmpty()) {
+                print(output, "Please enter a valid amount.");
+                return;
+            }
+            Double amount = Double.parseDouble(amountStr);
+
+            //run action
             switch (action) {
-                case "Open" -> {}
+                case "Open" -> OpenAccount(
+                        firstName,
+                        lastName,
+                        dob,
+                        accountType,
+                        campus,
+                        isLoyal,
+                        amount
+                );
                 case "Close" -> {}
                 case "Deposit" -> {}
                 case "Withdraw" -> {}
@@ -190,16 +219,61 @@ public class TransactionManagerController {
         }
     }
 
+    //*** helper methods ***//
+
+    /**
+     * Print out a string by appending it to a TEXT AREA
+     * @param output the TextArea to be appended to
+     * @param string the string to be printed
+     */
     public void print(TextArea output, String string ) {
         output.appendText(string + "\n");
     }
 
-    public void printFormatted(TextArea output, String formattedString, Object ... args) {
-        String outputString = String.format(formattedString, args) + "\n";
+    /**
+     * Print out a string by appending it to a TEXT AREA
+     * @param output the TextArea to be appended to
+     * @param formatString the string to be formatted and printed
+     * @param args the arguments used to format the string
+     */
+    public void printFormatted(TextArea output, String formatString, Object ... args) {
+        String outputString = String.format(formatString, args) + "\n";
         output.appendText(outputString);
     }
 
-    //** methods for handling creation, deletion, update **//
+    /**
+     * Print some account status
+     *
+     * @param profile     the profile of the account
+     * @param accountType the account type
+     * @param status      the status to print afterwards
+     */
+    private void printStatus(
+            Profile profile, String accountType, String status
+    ) {
+        printFormatted(output, "%s %s %s(%s) %s",
+                profile.getFname(),
+                profile.getLname(),
+                profile.getDob(),
+                accountType,
+                status);
+    }
+
+    /**
+     * Print that the account is opened, or it's already in database
+     *
+     * @param profile     the profile of the account
+     * @param accountType the account type
+     */
+    private void printOpenStatus(
+            Profile profile, String accountType, boolean opened
+    ) {
+        String status;
+        if (opened) status = "opened.";
+        else status = "is already in the database.";
+        printStatus(profile, accountType, status);
+    }
+
 
     /**
      * Create and return Date object based on mm/dd/yyyy string input.
@@ -243,6 +317,161 @@ public class TransactionManagerController {
             return false;
         }
         return true;
+    }
+
+    //*** methods for opening an account ***//
+
+    /**
+     * Create checking account
+     *
+     * @param firstName      first name of user
+     * @param lastName       last name of user
+     * @param dateOfBirth    DOB of user, in past
+     * @param initialDeposit initial deposit for account
+     */
+    private void OpenCheckingAccount(
+            String firstName,
+            String lastName,
+            Date dateOfBirth,
+            double initialDeposit
+    ) {
+        Profile profile = new Profile(firstName, lastName, dateOfBirth);
+        Checking account = new Checking(profile, initialDeposit);
+        boolean openSuccess = database.open(account);
+        printOpenStatus(profile, "C", openSuccess);
+    }
+
+    /**
+     * Create college checking account
+     *
+     * @param firstName      first name of user
+     * @param lastName       last name of user
+     * @param dateOfBirth    DOB of user, in past
+     * @param initialDeposit initial deposit for account
+     * @param campus     string of campus
+     */
+    private void OpenCollegeCheckingAccount(
+            String firstName,
+            String lastName,
+            Date dateOfBirth,
+            double initialDeposit,
+            String campus
+    ) {
+        if (dateOfBirth.getYearsSince() > 24){
+            print(output, "DOB invalid: " + dateOfBirth + " over 24.");
+            return;
+        }
+        Campus campusObj;
+        switch (campus) {
+            case "New Brunswick" -> campusObj = Campus.NEW_BRUNSWICK;
+            case "Newark" -> campusObj = Campus.NEWARK;
+            case "Camden" -> campusObj = Campus.CAMDEN;
+            default -> {
+                //unlikely to happen
+                print(output, "Invalid campus.");
+                return;
+            }
+        }
+        Profile profile = new Profile(firstName, lastName, dateOfBirth);
+        CollegeChecking account = new CollegeChecking(profile,
+                initialDeposit, campusObj);
+        boolean openSuccess = database.open(account);
+        printOpenStatus(profile, "CC", openSuccess);
+    }
+
+    /**
+     * Create savings account
+     *
+     * @param firstName      first name of user
+     * @param lastName       last name of user
+     * @param dateOfBirth    DOB of user, in past
+     * @param initialDeposit initial deposit for account
+     * @param isLoyal boolean based on whether customer is loyal
+     */
+    private void OpenSavingsAccount(
+            String firstName,
+            String lastName,
+            Date dateOfBirth,
+            double initialDeposit,
+            boolean isLoyal
+    ) {
+        Profile profile = new Profile(firstName, lastName, dateOfBirth);
+        Savings account = new Savings(profile, initialDeposit, isLoyal);
+        boolean openSuccess = database.open(account);
+        printOpenStatus(profile, "S", openSuccess);
+    }
+
+    /**
+     * Create money market account
+     *
+     * @param firstName      first name of user
+     * @param lastName       last name of user
+     * @param dateOfBirth    DOB of user, in past
+     * @param initialDeposit initial deposit for account
+     */
+    private void OpenMoneyMarketAccount(
+            String firstName,
+            String lastName,
+            Date dateOfBirth,
+            double initialDeposit
+    ) {
+        if (initialDeposit < 2000.00) {
+            print(output,
+                    "Minimum of $2000 to open a Money Market account."
+            );
+            return;
+        }
+        Profile profile = new Profile(firstName, lastName, dateOfBirth);
+        MoneyMarket account = new MoneyMarket(profile, initialDeposit,
+                true, 0);
+        boolean openSuccess = database.open(account);
+        printOpenStatus(profile, "MM", openSuccess);
+    }
+
+    /**
+     * Creates an account
+     * @param firstName string first name
+     * @param lastName string last name
+     * @param dob date of birth
+     * @param accountType type of account
+     * @param campus string campus
+     * @param isLoyal boolean is loyal
+     * @param initialDeposit initial deposit as double
+     */
+    private void OpenAccount(
+            String firstName,
+            String lastName,
+            Date dob,
+            String accountType,
+            String campus,
+            boolean isLoyal,
+            Double initialDeposit
+    ) {
+            if (dob.getYearsSince() < 16){
+                print(output, "DOB invalid: " + dob + " under 16.");
+                return;
+            }
+
+            if (initialDeposit <= 0) {
+                print(output,"Initial deposit cannot be 0 or negative" +
+                        ".");
+                return;
+            }
+
+            switch (accountType) {
+                case "Checking" -> OpenCheckingAccount(
+                        firstName, lastName, dob, initialDeposit
+                );
+                case "College Checking" -> OpenCollegeCheckingAccount(
+                        firstName, lastName, dob, initialDeposit, campus
+                );
+                case "Savings" -> OpenSavingsAccount(
+                        firstName, lastName, dob, initialDeposit, isLoyal
+                );
+                case "Money Market" -> OpenMoneyMarketAccount(
+                        firstName, lastName, dob, initialDeposit
+                );
+            }
     }
 
 
